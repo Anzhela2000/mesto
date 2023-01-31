52f2855ae363b6be4f5807c29f618bb5db065e5c
Anzhela A <baronina.anzhela2017@yandex.ru>
2023-01-31 14:40:13 +0300

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -12,13 +12,21 @@ handleCloseButton = () => {
 }
 
 editButton.addEventListener('click', handleEditButtonClick);
-closeButton.addEventListener('click' , handleCloseButton)
+closeButton.addEventListener('click', handleCloseButton);
 
 
 let popupSave = document.querySelector('.popup__button');
-let popupName = document.querySelector('.input__name');
-let popupJob = document.querySelector('.input__job');
-console.log(popupJob)
+let popupName = document.querySelector('.popup__name');
+let popupJob = document.querySelector('.popup__job');
+let profileName = document.querySelector('.profile__name')
+let profileButton = document.querySelector('.profile__job')
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
     evt.preventDefault();
+    profileName.textContent = popupName.value
+    profileButton.textContent = popupJob.value
+}
+
+popup.addEventListener('submit', handleFormSubmit);
+
+popupSave.addEventListener('click', handleCloseButton);
