035421d462c89bd009e35a519d09dd4a2236132c
Anzhela A <baronina.anzhela2017@yandex.ru>
2023-02-04 15:21:13 +0300

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
 let buttonChange = document.querySelector('.profile__button-change');
 let formElement = document.querySelector('.popup__container');
-let popup = document.querySelector('.popup')
+let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
-let profileName = document.querySelector('.profile__name')
-let profileButton = document.querySelector('.profile__job')
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileButton = document.querySelector('.profile__job');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileButton.textContent;
 }
 
 function closePopup() {
@@ -20,7 +22,7 @@ function saveInfoPopup(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileButton.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
 
 buttonChange.addEventListener('click', openPopup);
